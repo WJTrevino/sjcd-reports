6637a9df935673e0270b291fcd189ab2b9aed500
WJTMath <Westley.Trevino@sjcd.edu>
2022-05-24 13:37:06 -0500

    Prep for new .DoAnalysis
    Cleaning up code and old errors

--- shiny/app.R
+++ shiny/app.R
@@ -1,13 +1,15 @@
 # Load Dependencies ==========================================================
-library(jsonlite)
-library(magrittr)
-library(readxl)
+library("jsonlite")
+library("multcomp")
+library("magrittr")
+library("readxl")
 # trim when shipping
-library(tidyverse)
+library("tidyverse")
+library("glue")
 # load shiny last so it is not masked
-library(shiny)
-library(shinydashboard)
-library(shinydashboardPlus)
+library("shiny")
+library("shinydashboard")
+library("shinydashboardPlus")
 
 # Load Helpers ===============================================================
 source("helpers.R")
@@ -21,7 +23,8 @@ ui <- shiny::fluidPage(
     shiny::textInput("file","Mock file upload (File path)"),
     shiny::conditionalPanel(
         condition = "input.file",
-        #shiny::conditionalPanel(
+        shiny::conditionalPanel(
+          condition = "1 == 1",
           #condition = "output.dataValid == '1'",
           shiny::h4("About This Exam"),
           shiny::div(
@@ -37,24 +40,29 @@ ui <- shiny::fluidPage(
             shiny::textOutput("nQuestions", inline = TRUE)
           ),
           shiny::div(
-            "Questions: ",
+            "Goals: ",
             shiny::textOutput("nGoals", inline = TRUE)
           ),
           shiny::actionButton(
-            "doAnalysis",
-            "Run Analysis",
+            "doSummaryAnalysis",
+            "Run Summary",
+            icon = shiny::icon("calculator")
+          ),
+          shiny::br(),
+          shiny::actionButton(
+            "doQuestionAnalysis",
+            "Run Question Analysis",
             icon = shiny::icon("calculator")
           )
-        #)
+      )
     ),
     shiny::helpText(shiny::textOutput("status"))
   ),
   shiny::mainPanel(
     shiny::textOutput("file"),
-    shiny::verbatimTextOutput("sqlData"),
     shiny::verbatimTextOutput("joinedData"),
     shiny::verbatimTextOutput("helper"),
-    shiny::uiOutput("groups")
+    shiny::uiOutput("detailOutput")
   )
 )
 
@@ -70,6 +78,7 @@ server <- function(input, output, session) {
       "Summary Statistics",
       "Item Analysis",
       "Student Questions",
+      "Student Goals",
       "Goals Summary"
     )
     progress <- shiny::Progress$new(max = length(sheetNames))
@@ -103,6 +112,8 @@ server <- function(input, output, session) {
     )
     d <- data()
     m <- "Invalid Formatting"
+    
+    # List of formatting checks to verify EAC file.
     shiny::validate(
       shiny::need(d$'Summary Statistics'[[2,1]] == "Scorable Questions", m),
       shiny::need(d$'Summary Statistics'[[2,2]] > 0, m)
@@ -124,9 +135,15 @@ server <- function(input, output, session) {
   output$sqlData <- shiny::renderPrint({ sqlData() })
   
   joinedData <- shiny::reactive({
-    d <- shiny::req(data()$'Student Questions')
-    s <- shiny::req(sqlData())
-    dplyr::left_join(d, s, by = c("Student_id" = "sid")) %>%
+    item_sheet <- shiny::req(data()$'Student Questions')
+    goal_names <- shiny::req(data()$'Goals Summary'$Goals)
+    goal_sheet <- shiny::req(data()$'Student Goals') %>%
+      dplyr::select(dplyr::any_of(c("Student_id", goal_names)))
+    sql_data <- shiny::req(sqlData())
+    
+    dplyr::left_join(item_sheet, sql_data, by = c("Student_id" = "sid")) %>%
+      dplyr::left_join(., goal_sheet, by = "Student_id") %>%
+      dplyr::filter(!is.na(Student_id)) %>%
       purrr::modify_if(is.character, as.factor) %>%
       purrr::modify_at("Student_id", as.character) %>%
       dplyr::mutate(
@@ -138,33 +155,48 @@ server <- function(input, output, session) {
 
   output$examName <- shiny::renderText({
     shiny::req(dataValid())
-    d <- data()
     sub(" (**Webcam**) - Requires Respondus LockDown Browser",
         "",
-        d$`Test Info`[1,1],
+        data()$'Test Info'[[1,1]],
         fixed = TRUE)
   })
-  
+
+  nStudents <- shiny::renderText({
+    shiny::req(dataValid())
+    data()$'Summary Statistics'[[1,2]]
+  })
+  output$nStudents <- shiny::renderText({ nStudents() })
+    
   nQuestions <- shiny::renderText({
     shiny::req(dataValid())
-    d <- data()
-    d$'Summary Statistics'[[2,2]]
+    data()$'Summary Statistics'[[2,2]]
   })
   output$nQuestions <- shiny::renderText({ nQuestions() })
+  
+  nGoals <- shiny::renderText({
+    shiny::req(dataValid())
+    data()$'Summary Statistics'[[2,2]]
+  })
+  output$nGoals <- shiny::renderText({ nQuestions() })
   
   output$file <- shiny::renderText({
     input$file
   })
   
   output$helper <- shiny::renderPrint({
-    .DoAnalysis(joinedData(), "Total", "race")
+    joinedData() %>% names
+    #.DoAnalysis(joinedData(), "Total", "race")
   })
   
   output$status <- shiny::renderPrint({ validateData() })
   
-  output$groups <- shiny::renderUI({
+  output$detailOutput <- shiny::renderUI({
+    shiny::req(joinedData())
     lapply(1:nQuestions(), function(x) {
       shiny::h4(x)
+      #shiny::div(renderPrint({
+      #  .DoAnalysis(joinedData(), as.character(x), "race")
+      #}))
     })
   })
 }
--- shiny/helpers.R
+++ shiny/helpers.R
@@ -1,16 +1,30 @@
 # Helper Functions for DEI Reports ============================================
 # Dependencies are only run if debugging this file directly on a console.
 if(interactive()) {
-  library(shiny)
-  library(jsonlite)
-  library(magrittr)
-  library(readxl)
+  library("jsonlite")
+  library("multcomp")
+  library("magrittr")
+  library("readxl")
   # trim when shipping
-  library(tidyverse)
+  library("tidyverse")
   # load shiny last so it is not masked
-  library(shiny)
-  library(shinydashboard)
-  library(shinydashboardPlus)
+  library("shiny")
+  library("shinydashboard")
+  library("shinydashboardPlus")
+}
+
+.FilterData <- function(data, y, x) {
+  data %<>%
+    dplyr::filter(!is.na(.data[[y]])) %>%
+    dplyr::mutate("{x}" := forcats::fct_infreq(.data[[x]])) %>%
+    dplyr::mutate("{x}" := fct_lump_min(.data[[x]], min = 3))
+  
+  if ("Other" %in% levels(data[[x]])) {
+    data %>%
+      dplyr::mutate("{x}" := fct_relevel(.data[[x]], "Other", after = Inf))
+  }
+  
+  return(data)
 }
 
 .Summarize <- function(data, y, x) {
@@ -27,17 +41,17 @@ if(interactive()) {
 .QPEstimateEffects <- function(model, x) {
   v <- coef(model)
   names(v) <- sub(x, "", names(v))
-  est <- outer(v, v, function(x,y) { (exp(x-y)-1)*100 })
+  est <- outer(v, v, function(x,y) { round((exp(x-y)-1)*100, digits = 1) })
   tibble::as_tibble(est) %>%
     tibble::add_column(Group = names(v), .before = 1)
 }
 
 
-.TPValues = function(data, x, y) {
-  test <- pairwise.t.test(data[[x]], data[[y]])
+.TPValues = function(data, y, x) {
+  test <- pairwise.t.test(data[[as.character(y)]], data[[x]])
   p <- test$p.value
-  last_group <- rownames(p)[-1]
-  p %<>%
+  last_group <- tail(rownames(p), 1)
+  p %>%
     tibble::as_tibble(.) %>%
     tibble::add_row(.before = 1) %>%
     tibble::add_column({{ last_group }} := 0, .after = Inf) %>%
@@ -55,21 +69,18 @@ if(interactive()) {
     lens = x
   )
   
-  data %<>%
-    dplyr::filter(!is.na(.data[[y]])) %>%
-    dplyr::mutate("{x}" := forcats::fct_infreq(.data[[x]])) %>%
-    dplyr::mutate("{x}" := fct_lump_min(.data[[x]], min = 3)) %>%
-    # FIND A WAY TO MOVE OTHER TO END WITHOUT ERROR
-    dplyr::mutate("{x}" := fct_relevel(.data[[x]], "Other", after = Inf))
+  data %<>% .FilterData(., y, x)
   
   result$summary <-  .Summarize(data, y, x)
   
   data %<>%
     dplyr::filter(.data[[x]] != "Other")
   
-  if(nrow(result[["summary"]]) > 1) {
-    frm <- reformulate(x, response = y, intercept = FALSE)
-    model <- glm(frm, quasipoisson(link =), data = data)
+  if(nrow(result$summary) > 1) {
+    # Allow for numeric response variable names.
+    frm <- stats::as.formula(stringr::str_glue("`{y}` ~ {x} - 1"))
+    #model <- glm(frm, binomial, data = data)
+    model <- glm(frm, quasipoisson(link = log), data = data)
     effects <- .QPEstimateEffects(model, x)
     pvalues <- .TPValues(data, y, x)
     
@@ -80,5 +91,8 @@ if(interactive()) {
     result$message = "Cannot run analysis with only one significant group."
   }
   
-  result
+  return(result)
+  
+  # staging for future version of .DoAnalysis
+  
 }
